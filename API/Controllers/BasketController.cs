5c3dd5d993f31aa0010e5ff2d879d12012f03979
bukovskiy7777 <bukovskiy7777@gmail.com>
2022-03-07 10:50:44 +0200

    End of section 16 - API - Validation

--- API/Controllers/BasketController.cs
+++ API/Controllers/BasketController.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using API.DTOs;
+using AutoMapper;
 using Core.Entities;
 using Core.Interfaces;
 using Microsoft.AspNetCore.Mvc;
@@ -11,8 +13,10 @@ namespace API.Controllers
     public class BasketController : BaseApiController
     {
         private readonly IBasketRepository basketRepository;
-        public BasketController(IBasketRepository basketRepository)
+        private readonly IMapper mapper;
+        public BasketController(IBasketRepository basketRepository, IMapper mapper)
         {
+            this.mapper = mapper;
             this.basketRepository = basketRepository;
         }
 
@@ -25,9 +29,11 @@ namespace API.Controllers
         }
 
         [HttpPost]
-        public async Task<ActionResult<CustomerBasket>> UpdateBasket(CustomerBasket basket)
+        public async Task<ActionResult<CustomerBasket>> UpdateBasket(CustomerBasketDto basket)
         {
-            var updateBasket = await basketRepository.UpdateBasketAsync(basket);
+            var customerBasket = mapper.Map<CustomerBasketDto, CustomerBasket>(basket);
+
+            var updateBasket = await basketRepository.UpdateBasketAsync(customerBasket);
 
             return Ok(updateBasket);
         }
--- API/Extensions/SwaggerServiceExtensions.cs
+++ API/Extensions/SwaggerServiceExtensions.cs
@@ -12,7 +12,29 @@ namespace API.Extensions
     {
         public static IServiceCollection AddSwaggerDocumentation (this IServiceCollection services)
         {
-            services.AddSwaggerGen(c =>c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" }));
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
+
+                var securityScheme = new OpenApiSecurityScheme 
+                {
+                    Description = "JWT Auth Bearer Scheme",
+                    Name = "Authorization",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.Http,
+                    Scheme = "bearer",
+                    Reference = new OpenApiReference
+                    {
+                        Type = ReferenceType.SecurityScheme,
+                        Id = "Bearer"
+                    }
+                };
+
+                c.AddSecurityDefinition("Bearer", securityScheme);
+
+                var securityRequirement = new OpenApiSecurityRequirement {{securityScheme, new [] {"Bearer"}}};
+                c.AddSecurityRequirement(securityRequirement);
+            });
 
             return services;
         }
